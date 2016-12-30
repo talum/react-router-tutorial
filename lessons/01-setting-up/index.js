542a797e362d2057c517f09ed5901562b77026ee
talum <tracy.a.lum@gmail.com>
2016-12-30 11:13:30 -0500

    playin with router

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, browserHistory, IndexRoute} from 'react-router'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import Repos from './modules/Repos'
+import About from './modules/About'
+import Repo from './modules/Repo'
+import Home from './modules/Home'
+
+render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="/repos" component={Repos}>
+        <Route path="/repos/:userName/:repoName" component={Repo}/>
+      </Route>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+  ), document.getElementById('app'))
